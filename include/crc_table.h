9ce453b6014a803f68a95d664e9439ead44796a9
Postrantor <postrantor@gmail.com>
2024-12-17 23:25:34 +0800

    [imu] format code

--- include/crc_table.h
+++ include/crc_table.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 
-uint8_t CRC8_Table(uint8_t* p, uint8_t counter);
+uint8_t CRC8_Table(uint8_t *p, uint8_t counter);
 uint16_t CRC16_Table(uint8_t *p, uint8_t counter);
 uint32_t CRC32_Table(uint8_t *p, uint8_t counter);
 
